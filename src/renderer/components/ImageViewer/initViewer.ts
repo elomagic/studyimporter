7cc36d231debfedc908464a0bba9de263503d9c3
Carsten Rambow <dev@elomagic.de>
2024-05-13 13:49:26 +0200

    Customized provider removed

--- src/renderer/components/ImageViewer/initViewer.ts
+++ src/renderer/components/ImageViewer/initViewer.ts
@@ -1,11 +1,9 @@
 import { init as csToolsInit } from '@cornerstonejs/tools';
 import { init as csRenderInit } from '@cornerstonejs/core';
-import initProviders from './initProviders';
 import initVolumeLoader from './initVolumeLoader';
 import initImageLoader from './initImageLoader';
 
 export default async function initViewer() {
-  initProviders();
   initImageLoader();
   initVolumeLoader();
 
