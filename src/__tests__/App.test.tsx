f400ce3b317757cbac6a3c893ad344b3d7e488ef
C. Rambow <rambow.dev@elomagic.de>
2024-06-25 18:31:16 +0200

    Electron 28 breaks build. Fixed?

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,16 +1,14 @@
 import '@testing-library/jest-dom';
-// import { render } from '@testing-library/react';
-// import App from '../renderer/App';
+import { render } from '@testing-library/react';
+import App from '../renderer/App';
 
 describe('App', () => {
   it('dummy test', () => {
     expect(1 + 2).toBe(3);
   });
 
-  /* TODO Added by Carsten on 2024-12-04. Currently, module @cornerstonejs makes trouble, so we have to disable test in the meanwhile
+  // TODO Added by Carsten on 2024-12-04. Currently, module @cornerstonejs makes trouble, so we have to disable test in the meanwhile
   it('should render', () => {
     expect(render(<App />)).toBeTruthy();
   });
-
-   */
 });
